b2925ee0017f2abe4063ab3050ca724d636d982a
memoryleak47 <memoryleak47@protonmail.ch>
2018-09-23 10:07:17 +0200

    worked on polygon.rs

--- src/polygon.rs
+++ src/polygon.rs
@@ -13,7 +13,7 @@ pub struct PolygonFrame {
 }
 
 pub struct ChunkWeb {
-	chunks: Vec<(Vec3f, Chunk)>, // TODO user better representation
+	chunks: Vec<(Vec3f, Chunk)>, // TODO use better representation
 }
 
 pub enum Chunk {
@@ -27,10 +27,7 @@ pub enum Chunk {
 
 impl PolygonFrame {
 	pub fn build(frame: &Frame) -> PolygonFrame {
-		let x = frame.player.position_x;
-		let z = frame.player.position_z;
-		let y = frame.get_height(x, z);
-		let camera_position = Vec3f::new(x, y, z);
+		let camera_position = PolygonFrame::get_camera(frame);
 
 		PolygonFrame {
 			camera_position,
@@ -41,25 +38,44 @@ impl PolygonFrame {
 	}
 
 	pub fn update(&mut self, frame: &Frame) {
-		// TODO
+		let camera_position = PolygonFrame::get_camera(frame);
+
+		self.chunk_web.update(frame, camera_position, self.camera_position);
+
+		self.camera_position = camera_position;
+		self.jaw =  frame.player.jaw;
+		self.pitch =  frame.player.pitch;
+	}
+
+	fn get_camera(frame: &Frame) -> Vec3f {
+		let x = frame.player.position_x;
+		let z = frame.player.position_z;
+		let y = frame.get_height(x, z);
+
+		Vec3f::new(x, y, z)
 	}
 }
 
 impl ChunkWeb {
-	fn build(frame: &Frame, source: Vec3f) -> ChunkWeb {
+	fn build(frame: &Frame, camera: Vec3f) -> ChunkWeb {
 		let mut chunks = Vec::new();
-		for x in ChunkWeb::get_relevant_sources(source).into_iter() {
-			chunks.push((x, Chunk::build(frame, x, BASE_CHUNK_SIZE)));
+		for x in ChunkWeb::get_relevant_sources(camera).into_iter() {
+			chunks.push((x, Chunk::build(frame, x, BASE_CHUNK_SIZE, camera)));
 		}
 		ChunkWeb { chunks }
 		
 	}
 
-	fn update(&mut self, frame: &Frame, source: Vec3f) {
-		// TODO
+	fn update(&mut self, frame: &Frame, new_camera: Vec3f, old_camera: Vec3f) {
+		// TODO:
+		// - detect newly combined Chunks
+		// - detect newly splat Chunks
+		// - detect new base chunks
+		// - detect deprecated base chunks
+		// - apply these changes
 	}
 
-	fn get_relevant_sources(pos: Vec3f) -> Vec<Vec3f> {
+	fn get_relevant_sources(camera: Vec3f) -> Vec<Vec3f> {
 		let round = |p: Vec3f| {
 			Vec3f::new (
 				p.x % BASE_CHUNK_SIZE,
@@ -74,7 +90,7 @@ impl ChunkWeb {
 		for &x in [-BASE_CHUNK_SIZE, 0., BASE_CHUNK_SIZE].into_iter() {
 			for &y in [-BASE_CHUNK_SIZE, 0., BASE_CHUNK_SIZE].into_iter() {
 				for &z in [-BASE_CHUNK_SIZE, 0., BASE_CHUNK_SIZE].into_iter() {
-					let moved_pos: Vec3f = round(pos + Vec3f::new(x, y, z));
+					let moved_pos: Vec3f = round(camera + Vec3f::new(x, y, z));
 					if !v.contains(&moved_pos) {
 						v.push(moved_pos);
 					}
@@ -87,7 +103,7 @@ impl ChunkWeb {
 }
 
 impl Chunk {
-	fn build(frame: &Frame, from: Vec3f, chunk_size: f32) -> Chunk {
+	fn build(frame: &Frame, from: Vec3f, chunk_size: f32, camera: Vec3f) -> Chunk {
 		// TODO
 		unimplemented!()
 	}
