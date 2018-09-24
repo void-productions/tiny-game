0f7a36910346815323a51fde76d2e711be08dff9
memoryleak47 <memoryleak47@protonmail.ch>
2018-09-24 21:13:20 +0200

    reworked vec modules

--- src/vec/vec2t.rs
+++ src/vec/vec2t.rs
@@ -1,36 +1,44 @@
 use std::ops::{Add, Sub, Mul, Div};
+use std::fmt::{Display, Debug, Error, Formatter};
+use std::hash::{Hash, Hasher};
 
-#[derive(Clone, Debug, PartialEq)]
-pub struct Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+pub struct Vec2t<T> {
 	pub x: T,
 	pub y: T,
 }
 
 pub type Vec2f = Vec2t<f32>;
-impl Copy for Vec2f {}
+pub type Vec2u = Vec2t<u32>;
 
-impl<T> Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Vec2t<T> {
 	pub fn new(x: T, y: T) -> Vec2t<T> {
 		Vec2t { x, y }
 	}
 }
 
-impl<T> Add<Vec2t<T>> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Add<Vec2t<T>> for Vec2t<T> where T: Add<Output=T> {
 	type Output = Vec2t<T>;
 
-	fn add(self, rhs: Vec2t<T>) -> Vec2t<T> {
+	fn add(self, other: Vec2t<T>) -> Vec2t<T> {
 		Vec2t::new (
-			self.x + rhs.x,
-			self.y + rhs.y
+			self.x + other.x,
+			self.y + other.y,
 		)
 	}
 }
 
-impl<T> Sub<Vec2t<T>> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Add<T> for Vec2t<T> where T: Add<Output=T> + Copy {
+	type Output = Vec2t<T>;
+
+	fn add(self, other: T) -> Vec2t<T> {
+		Vec2t::new (
+			self.x + other,
+			self.y + other,
+		)
+	}
+}
+
+impl<T> Sub<Vec2t<T>> for Vec2t<T> where T: Sub<Output=T> {
 	type Output = Vec2t<T>;
 
 	fn sub(self, other: Vec2t<T>) -> Vec2t<T> {
@@ -41,8 +49,18 @@ impl<T> Sub<Vec2t<T>> for Vec2t<T>
 	}
 }
 
-impl<T> Mul<Vec2t<T>> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Sub<T> for Vec2t<T> where T: Sub<Output=T> + Copy {
+	type Output = Vec2t<T>;
+
+	fn sub(self, other: T) -> Vec2t<T> {
+		Vec2t::new (
+			self.x - other,
+			self.y - other,
+		)
+	}
+}
+
+impl<T> Mul<Vec2t<T>> for Vec2t<T> where T: Mul<Output=T> {
 	type Output = Vec2t<T>;
 
 	fn mul(self, other: Vec2t<T>) -> Vec2t<T> {
@@ -53,8 +71,7 @@ impl<T> Mul<Vec2t<T>> for Vec2t<T>
 	}
 }
 
-impl<T> Mul<T> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Mul<T> for Vec2t<T> where T: Mul<Output=T> + Copy {
 	type Output = Vec2t<T>;
 
 	fn mul(self, other: T) -> Vec2t<T> {
@@ -65,8 +82,7 @@ impl<T> Mul<T> for Vec2t<T>
 	}
 }
 
-impl<T> Div<Vec2t<T>> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Div<Vec2t<T>> for Vec2t<T> where T: Div<Output=T> {
 	type Output = Vec2t<T>;
 
 	fn div(self, other: Vec2t<T>) -> Vec2t<T> {
@@ -77,8 +93,7 @@ impl<T> Div<Vec2t<T>> for Vec2t<T>
 	}
 }
 
-impl<T> Div<T> for Vec2t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Div<T> for Vec2t<T> where T: Div<Output=T> + Copy {
 	type Output = Vec2t<T>;
 
 	fn div(self, other: T) -> Vec2t<T> {
@@ -86,5 +101,55 @@ impl<T> Div<T> for Vec2t<T>
 			self.x / other,
 			self.y / other,
 		)
+	}
+}
+
+impl<T> Hash for Vec2t<T> where T: Hash {
+	fn hash<H: Hasher>(&self, h: &mut H) {
+		self.x.hash(h);
+		self.y.hash(h);
+		h.finish();
+	}
+}
+
+impl<T: PartialEq> PartialEq for Vec2t<T> {
+	fn eq(&self, rhs: &Self) -> bool {
+		(self.x == rhs.x) && (self.y == rhs.y)
+	}
+}
+
+impl<T: Eq> Eq for Vec2t<T> {}
+
+impl<T> Vec2t<T> {
+	pub fn map<U, F: Fn(T) -> U>(self, f: F) -> Vec2t<U> {
+		Vec2t::new(
+			f(self.x),
+			f(self.y),
+		)
+	}
+}
+
+impl<T: Clone> Clone for Vec2t<T> {
+	fn clone(&self) -> Self {
+		Vec2t::new(
+			self.x.clone(),
+			self.y.clone(),
+		)
+	}
+}
+
+impl<T: Copy> Copy for Vec2t<T> { }
+
+impl<T: Display> Display for Vec2t<T> {
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+		let s = format!("Vec2t({}, {})", self.x, self.y);
+		fmt.write_str(&*s)
+	}
+}
+
+impl<T: Debug> Debug for Vec2t<T> {
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+		let s = format!("Vec2t({:?}, {:?})", self.x, self.y);
+		fmt.write_str(&*s)
 	}
 }
--- src/vec/vec3t.rs
+++ src/vec/vec3t.rs
@@ -1,8 +1,8 @@
 use std::ops::{Add, Sub, Mul, Div};
+use std::fmt::{Display, Debug, Error, Formatter};
+use std::hash::{Hash, Hasher};
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+pub struct Vec3t<T> {
 	pub x: T,
 	pub y: T,
 	pub z: T,
@@ -11,93 +11,25 @@ pub struct Vec3t<T>
 pub type Vec3f = Vec3t<f32>;
 pub type Vec3u = Vec3t<u32>;
 
-impl<T> Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Vec3t<T> {
 	pub fn new(x: T, y: T, z: T) -> Vec3t<T> {
 		Vec3t { x, y, z }
 	}
 }
 
-impl<T> Add<Vec3t<T>> for Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Add<Vec3t<T>> for Vec3t<T> where T: Add<Output=T> {
 	type Output = Vec3t<T>;
 
-	fn add(self, rhs: Vec3t<T>) -> Vec3t<T> {
+	fn add(self, other: Vec3t<T>) -> Vec3t<T> {
 		Vec3t::new (
-			self.x + rhs.x,
-			self.y + rhs.y,
-			self.z + rhs.z
+			self.x + other.x,
+			self.y + other.y,
+			self.z + other.z,
 		)
 	}
 }
 
-impl<T> Sub<Vec3t<T>> for Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
-	type Output = Vec3t<T>;
-
-	fn sub(self, other: Vec3t<T>) -> Vec3t<T> {
-		Vec3t::new (
-			self.x - other.x,
-			self.y - other.y,
-			self.z - other.z,
-		)
-	}
-}
-
-impl<T> Mul<Vec3t<T>> for Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
-	type Output = Vec3t<T>;
-
-	fn mul(self, other: Vec3t<T>) -> Vec3t<T> {
-		Vec3t::new (
-			self.x * other.x,
-			self.y * other.y,
-			self.z * other.z,
-		)
-	}
-}
-
-impl<T> Mul<T> for Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
-	type Output = Vec3t<T>;
-
-	fn mul(self, other: T) -> Vec3t<T> {
-		Vec3t::new (
-			self.x * other,
-			self.y * other,
-			self.z * other,
-		)
-	}
-}
-
-impl<T> Div<Vec3t<T>> for Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
-	type Output = Vec3t<T>;
-
-	fn div(self, other: Vec3t<T>) -> Vec3t<T> {
-		Vec3t::new (
-			self.x / other.x,
-			self.y / other.y,
-			self.z / other.z,
-		)
-	}
-}
-
-impl<T> Div<T> for Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
-	type Output = Vec3t<T>;
-
-	fn div(self, other: T) -> Vec3t<T> {
-		Vec3t::new (
-			self.x / other,
-			self.y / other,
-			self.z / other,
-		)
-	}
-}
-
-impl<T> Add<T> for Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Add<T> for Vec3t<T> where T: Add<Output=T> + Copy {
 	type Output = Vec3t<T>;
 
 	fn add(self, other: T) -> Vec3t<T> {
@@ -109,8 +41,19 @@ impl<T> Add<T> for Vec3t<T>
 	}
 }
 
-impl<T> Sub<T> for Vec3t<T>
-		where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+impl<T> Sub<Vec3t<T>> for Vec3t<T> where T: Sub<Output=T> {
+	type Output = Vec3t<T>;
+
+	fn sub(self, other: Vec3t<T>) -> Vec3t<T> {
+		Vec3t::new (
+			self.x - other.x,
+			self.y - other.y,
+			self.z - other.z,
+		)
+	}
+}
+
+impl<T> Sub<T> for Vec3t<T> where T: Sub<Output=T> + Copy {
 	type Output = Vec3t<T>;
 
 	fn sub(self, other: T) -> Vec3t<T> {
@@ -122,9 +65,55 @@ impl<T> Sub<T> for Vec3t<T>
 	}
 }
 
-use std::hash::{Hash, Hasher};
+impl<T> Mul<Vec3t<T>> for Vec3t<T> where T: Mul<Output=T> {
+	type Output = Vec3t<T>;
 
-impl<T> Hash for Vec3t<T> where T: Hash + Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
+	fn mul(self, other: Vec3t<T>) -> Vec3t<T> {
+		Vec3t::new (
+			self.x * other.x,
+			self.y * other.y,
+			self.z * other.z,
+		)
+	}
+}
+
+impl<T> Mul<T> for Vec3t<T> where T: Mul<Output=T> + Copy {
+	type Output = Vec3t<T>;
+
+	fn mul(self, other: T) -> Vec3t<T> {
+		Vec3t::new (
+			self.x * other,
+			self.y * other,
+			self.z * other,
+		)
+	}
+}
+
+impl<T> Div<Vec3t<T>> for Vec3t<T> where T: Div<Output=T> {
+	type Output = Vec3t<T>;
+
+	fn div(self, other: Vec3t<T>) -> Vec3t<T> {
+		Vec3t::new (
+			self.x / other.x,
+			self.y / other.y,
+			self.z / other.z,
+		)
+	}
+}
+
+impl<T> Div<T> for Vec3t<T> where T: Div<Output=T> + Copy {
+	type Output = Vec3t<T>;
+
+	fn div(self, other: T) -> Vec3t<T> {
+		Vec3t::new (
+			self.x / other,
+			self.y / other,
+			self.z / other,
+		)
+	}
+}
+
+impl<T> Hash for Vec3t<T> where T: Hash {
 	fn hash<H: Hasher>(&self, h: &mut H) {
 		self.x.hash(h);
 		self.y.hash(h);
@@ -133,14 +122,46 @@ impl<T> Hash for Vec3t<T> where T: Hash + Add<Output=T> + Sub<Output=T> + Mul<Ou
 	}
 }
 
-impl<T> Eq for Vec3t<T> where T: Hash + Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq + Eq {}
+impl<T: PartialEq> PartialEq for Vec3t<T> {
+	fn eq(&self, rhs: &Self) -> bool {
+		(self.x == rhs.x) && (self.y == rhs.y) && (self.z == rhs.z)
+	}
+}
 
-impl<T> Vec3t<T> where T: Add<Output=T> + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Copy + PartialEq {
-	pub fn map<U, F: Fn(T) -> U>(self, f: F) -> Vec3t<U> where U: Add<Output=U> + Sub<Output=U> + Mul<Output=U> + Div<Output=U> + Copy + PartialEq {
+impl<T: Eq> Eq for Vec3t<T> {}
+
+impl<T> Vec3t<T> {
+	pub fn map<U, F: Fn(T) -> U>(self, f: F) -> Vec3t<U> {
 		Vec3t::new(
 			f(self.x),
 			f(self.y),
 			f(self.z),
 		)
+	}
+}
+
+impl<T: Clone> Clone for Vec3t<T> {
+	fn clone(&self) -> Self {
+		Vec3t::new(
+			self.x.clone(),
+			self.y.clone(),
+			self.z.clone(),
+		)
+	}
+}
+
+impl<T: Copy> Copy for Vec3t<T> { }
+
+impl<T: Display> Display for Vec3t<T> {
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+		let s = format!("Vec3t({}, {}, {})", self.x, self.y, self.z);
+		fmt.write_str(&*s)
+	}
+}
+
+impl<T: Debug> Debug for Vec3t<T> {
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+		let s = format!("Vec3t({:?}, {:?}, {:?})", self.x, self.y, self.z);
+		fmt.write_str(&*s)
 	}
 }
