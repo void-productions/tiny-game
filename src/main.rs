432433ca6168eff888bbd3806f4a62a80ffebb60
Bluemi <bruno.schilling@protonmail.ch>
2020-05-05 16:38:41 +0200

    new beginning

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-#![deny(bare_trait_objects)]
-#![feature(nll)]
-
-#[macro_use]
-extern crate lazy_static;
-extern crate simdnoise;
-extern crate winit;
-
-mod vec;
-mod action;
-mod mapper;
-mod toggle;
-mod input;
-mod cycle;
-mod render;
-mod polygon;
-mod window_handler;
-mod core;
-mod physics;
-mod frame;
-
-use frame::Frame;
-
 fn main() {
-	let frame = Frame::new();
-	core::run(frame);
+    println!("hello world!");
 }
--- src/vec/mod.rs
+++
@@ -1,5 +0,0 @@
-mod vec2t;
-mod vec3t;
-
-pub use self::vec2t::*;
-pub use self::vec3t::*;
